17e5b7a9d6d697b2931e3e08264148df01d29272
rsdczhs <67510605+rsdczhs@users.noreply.github.com>
2021-01-13 20:51:59 +0800

    Update Net1814080903313Activity.java

--- students/net1814080903313/Net1814080903313Activity.java
+++ students/net1814080903313/Net1814080903313Activity.java
@@ -4,24 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
 public class Net1814080903313Activity extends AppCompatActivity {
-    private Button mBtA2;
+    private ImageView welcomeImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mBtA2 = findViewById(R.id.button);
-        mBtA2.setOnClickListener(new View.OnClickListener() {
+        welcomeImg=this.findViewById(R.id.img_1);
+        AlphaAnimation anima =  new AlphaAnimation(0.3f, 1.0f);
+        anima.setDuration(10000);// 设置动画显示时间
+        welcomeImg.startAnimation(anima);
+        anima.setAnimationListener(new Animation.AnimationListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Net1814080903313Activity.this,Activity2.class);
-                startActivity(intent);
+            public void onAnimationStart(Animation animation) {
+welcomeImg.setBackgroundResource(R.drawable.img1);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+skip();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
             }
         });
+    }
+
+
+    private void skip() {
+        Intent intent = new Intent(this,QueryFunction.class);
+        startActivity(intent);
+        finish();
     }
 }
